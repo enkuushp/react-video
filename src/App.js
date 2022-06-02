64ab09a26a29e0fce40ed3a554f51ab4d3e041c4
Enkhnaran Purevjav <p.enkhnaran@gmail.com>
2022-06-02 16:52:21 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import video from './assets/SPARKS_Banner_1440x754_web_x2Loop_noNoise.mp4';
+import videoMobile from './assets/SPARKS_Banner_414x684_mobile_x2LoopnoNoise.mp4';
+
 
 function App() {
+  const videClass = {
+    maxWidth: "100vw",
+    width: "100%",
+    height: "75vh",
+    minHeight: "650px",
+    maxHeight: "800px",
+    overflow: "hidden",
+    background: "#1b1b1b",
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <video className={videClass} autoPlay muted playsInline loop>
+        <source src={video} type="video/mp4" />
+        <track src="captions_en.vtt" kind="captions" srcLang="en" label="english_captions" />
+      </video>
+      <video className={videClass} autoPlay muted playsInline loop>
+        <source src={videoMobile} type="video/mp4" />
+        <track src="captions_en.vtt" kind="captions" srcLang="en" label="english_captions" />
+      </video>
     </div>
   );
 }
