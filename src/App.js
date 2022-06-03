782e7bad71ff732ac51004cf2897a6054fbc207f
Enkhnaran Purevjav <p.enkhnaran@gmail.com>
2022-06-02 17:28:32 -0700

    set dangerouslySetInnerHTML

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import videoMobile from './assets/SPARKS_Banner_414x684_mobile_x2LoopnoNoise.mp4';
 
 function App() {
-  const videClass = {
-    overflow: "hidden",
-    background: "#1b1b1b",
-  };
-
   return (
     <div>
       <div dangerouslySetInnerHTML={{
         __html: `
-          <video className=${videClass} autoPlay muted playsInline loop>
+          <video
+            loop
+            muted
+            autoplay
+            playsinline
+            preload="metadata"
+          >
             <source src=${videoMobile} type="video/mp4" />
-            <track src="captions_en.vtt" kind="captions" srcLang="en" label="english_captions" />
           </video>`
         }}
       />
