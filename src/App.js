e228165af80d8e86fc6e56ada40cde0f1adce692
Enkhnaran Purevjav <p.enkhnaran@gmail.com>
2022-06-02 17:10:38 -0700

    autoplay test

--- src/App.js
+++ src/App.js
@@ -1,40 +1,69 @@
-import video from './assets/SPARKS_Banner_1440x754_web_x2Loop_noNoise.mp4';
-import videoMobile from './assets/SPARKS_Banner_414x684_mobile_x2LoopnoNoise.mp4';
+import React, { useRef, useState, useEffect } from "react";
+import mainVideo from './assets/SPARKS_Banner_414x684_mobile_x2LoopnoNoise.mp4';
 
+const isSafari = () => {
+  const ua = navigator.userAgent.toLowerCase();
+  return ua.indexOf("safari") > -1 && ua.indexOf("chrome") < 0;
+};
 
-function App() {
-  const videClass = {
-    maxWidth: "100vw",
-    width: "100%",
-    height: "75vh",
-    minHeight: "650px",
-    maxHeight: "800px",
-    overflow: "hidden",
-    background: "#1b1b1b",
-  };
+// const mainVideo =
+//   "https://file-examples.com/wp-content/uploads/2017/04/file_example_MP4_480_1_5MG.mp4";
 
-  return (
-    <div>
-      <div
-        dangerouslySetInnerHTML={{
+export default function App() {
+  const videoParentRef = useRef();
+  const [shouldUseImage, setShouldUseImage] = useState(false);
+  useEffect(() => {
+    // check if user agent is safari and we have the ref to the container <div />
+    if (isSafari() && videoParentRef.current) {
+      // obtain reference to the video element
+      const player = videoParentRef.current.children[0];
+
+      // if the reference to video player has been obtained
+      if (player) {
+        // set the video attributes using javascript as per the
+        // webkit Policy
+        player.controls = false;
+        player.playsinline = true;
+        player.muted = true;
+        player.setAttribute("muted", ""); // leave no stones unturned :)
+        player.autoplay = true;
+
+        // Let's wait for an event loop tick and be async.
+        setTimeout(() => {
+          // player.play() might return a promise but it's not guaranteed crossbrowser.
+          const promise = player.play();
+          // let's play safe to ensure that if we do have a promise
+          if (promise.then) {
+            promise
+              .then(() => {})
+              .catch(() => {
+                // if promise fails, hide the video and fallback to <img> tag
+                videoParentRef.current.style.display = "none";
+                setShouldUseImage(true);
+              });
+          }
+        }, 0);
+      }
+    }
+  }, []);
+
+  return shouldUseImage ? (
+    <img src={mainVideo} alt="Muted Video" />
+  ) : (
+    <div
+      ref={videoParentRef}
+      dangerouslySetInnerHTML={{
         __html: `
-          <video className=${videClass} autoPlay muted playsInline loop>
-            <source src=${video} type="video/mp4" />
-            <track src="captions_en.vtt" kind="captions" srcLang="en" label="english_captions" />
-          </video>`
+        <video
+          loop
+          muted
+          autoplay
+          playsinline
+          preload="metadata"
+        >
+        <source src="${mainVideo}" type="video/mp4" />
+        </video>`
       }}
-      />
-      <div
-        dangerouslySetInnerHTML={{
-        __html: `
-          <video className=${videClass} autoPlay muted playsInline loop>
-            <source src=${videoMobile} type="video/mp4" />
-            <track src="captions_en.vtt" kind="captions" srcLang="en" label="english_captions" />
-          </video>`
-        }}
-      />
-    </div>
+    />
   );
 }
-
-export default App;
